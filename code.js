b72accbb87fb1a47ff3ca054c2671ad65798af72
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-06-25 10:48:57 -0500

    refactored

--- code.js
+++ code.js
@@ -39,3 +39,5 @@ function isStringTrue(input){
 }
 
 
+
+
--- tests.js
+++ tests.js
@@ -29,14 +29,28 @@ describe('sayHello', function() {
     it('should return the string "Hello, Pat!"', function(){
         expect(sayHelloPat('Pat')).toBe("Hello, Pat!");
     });
-    it('should return the string, "Hello, World"', function (){
-        expect(sayHello('World')).toBe("Hello, World!");
-    });
+    // it('should return the string, "Hello, World"', function (){
+    //     expect(sayHello('World')).toBe("Hello, World!");
+    // });
     it('the string, "Hello, World!" should return true', function(){
         expect(isStringTrue(true)).toBe("Hello, World!");
     });
     it('the string, "Hello, World!" should return false', function(){
         expect(isStringTrue(false)).toBe("Hello, World!");
-    })
-
+    });
+    it('if null return the string, "Hello, World!" if', function(){
+        expect(sayHello(null)).toBe("Hello, World!");
+    });
+    it('if empty string return, "Hello, World!"', function(){
+        expect(sayHello("")).toBe("Hello, World!");
+    });
+    it('if numeric return, "Hello, World!"', function(){
+        expect(sayHello(2.3)).toBe("Hello, World!");
+    });
+    it('if random input in the string, "Hello, World!"', function(){
+        expect(sayHello("5")).toBe("Hello, World!");
+    });
+    it('if an array return, "Hello, World!"', function(){
+        expect(sayHello([])).toBe("Hello, World!");
+    });
 });
